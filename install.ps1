aaedb06b997f20fb68e2cd9e5b0c09a3fe6d032e
James Ko <jamesqko@gmail.com>
2015-11-24 22:29:13 -0500

    String interpolation and better comments in install.ps1

--- install.ps1
+++ install.ps1
@@ -10,22 +10,24 @@ $urls = $latest.assets | Select browser_download_url
 $amd64 = [Environment]::Is64BitOperatingSystem
 $pattern = ('Win32', 'x64')[$amd64]
 
-$query = $urls | Where { $_ | Select-String $pattern }
+$query = $urls | Where { $_ | Select-String "Emptify.$pattern.zip" }
 $url = $query | Select -Index 0
 
 Invoke-WebRequest $url.browser_download_url -OutFile 'Emptify.zip'
 
-# Remove previous installations
+# Decompress the zip file
 $src = [IO.Path]::Combine($pwd.Path, 'Emptify.zip') # Path.GetFullPath is unreliable in PS-- see stackoverflow.com/q/33907574
 $dest = [IO.Path]::Combine($pwd.Path, 'Emptify')
+
+# Remove previous installations
 if ([IO.Directory]::Exists($dest))
 {
     [IO.Directory]::Delete($dest, $true)
 }
 
-# Decompress the zip file
-Add-Type -AssemblyName System.IO.Compression.FileSystem
-[IO.Compression.ZipFile]::ExtractToDirectory($src, $dest)
+Add-Type -AssemblyName System.IO.Compression.FileSystem # PowerShell lacks native support for zip files
+[IO.Compression.ZipFile]::ExtractToDirectory($src, $pwd.Path)
+Move-Item "Emptify.$pattern" 'Emptify' # Erase architecture info
 Remove-Item 'Emptify.zip' # Cleanup after ourselves
 
 # Add ourselves to PATH if not in it
@@ -33,8 +35,8 @@ Get-Command emptify 2> $null
 if (-not $?)
 {
     $current = [Environment]::GetEnvironmentVariable('PATH', [EnvironmentVariableTarget]::User)
-    $corrupt = $current.EndsWith(';')
-    $suffix = (';' + $dest, $dest)[$corrupt]
+    $corrupt = $current.EndsWith(';') # Don't add a ; to PATH if it already ends in one
+    $suffix = (";$dest", $dest)[$corrupt]
     $new = $current + $suffix
     [Environment]::SetEnvironmentVariable('PATH', $new, [EnvironmentVariableTarget]::User)
 }
